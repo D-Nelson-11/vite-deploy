28422d06508da1a1277d6c32bccb62f64dc15733
D-Nelson-11 <109240563+D-Nelson-11@users.noreply.github.com>
2024-01-10 09:23:19 -0600

    cambio

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/https://D-Nelson-11.github.io/vite-deploy/',
+  base: '/vite-deploy/',
 })
